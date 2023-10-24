902ff3762289306b564d6e9ceffbb365cc51f907
jmpark <pj.jmpark@kalmate.net>
2023-10-24 09:52:54 +0900

    소스 삭제

--- src/main/java/com/bdp/ap/common/EscapeHelper.java
+++
@@ -1,9 +0,0 @@
-package com.bdp.ap.common;
-import org.springframework.util.StringUtils;
-
-public class EscapeHelper {
-    public static String escape(String s) {
-        return StringUtils.hasText(s) ?
-                s.replaceAll("([%_])", "\\\\$1") : s;
-    }
-}
