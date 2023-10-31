caa9e170de54a3efaf0cd17d56934b916a07d48c
jmpark <pj.jmpark@kalmate.net>
2023-10-31 17:49:18 +0900

    ApiResDto 주석 수정

--- src/main/java/com/cdp/portal/common/dto/ApiResDto.java
+++ src/main/java/com/cdp/portal/common/dto/ApiResDto.java
@@ -18,7 +18,7 @@ import lombok.NoArgsConstructor;
  * API 공통 응답
  * success: request 기반 작업에 대해 모두 잘 진행되었으며, response data가 returned.
  * fail: request 때 제출된 데이터에 문제가 있거나, API 호출의 일부 조건에 대해 맞지 않음.
- * error: equest을 처리하는 중에 오류가 발생했습니다 (예 : exception 발생)
+ * error: request을 처리하는 중에 오류가 발생했습니다 (예 : exception 발생)
  * 
  * 참조: https://github.com/omniti-labs/jsend
  * @param <T>
