903dcf485334a80e01e9d3d6413c09dc58713399
이욱현 <pj.uhlee@kalmate.net>
2023-11-29 10:42:14 +0900

    부서관리 API 행번, 상위부서명 추가

--- src/main/java/com/cdp/portal/app/facade/dept/dto/response/DeptMgmtResDto.java
+++ src/main/java/com/cdp/portal/app/facade/dept/dto/response/DeptMgmtResDto.java
@@ -17,45 +17,51 @@ import lombok.ToString;
 @Schema(description = "부서관리 응답")
 public class DeptMgmtResDto {
 	public static class ApiResDepts extends ApiResDto<DeptMgmtResDto.DeptsResult> {}
-	
+
 	@Getter
     @Setter
     @ToString
     @NoArgsConstructor(access = AccessLevel.PROTECTED)
     @Schema(description = "부서 상세")
     public static class Dept {
-        
+
+		@Schema(description = "행번", nullable = false)
+    	private int rowNum;
+
         @Schema(description = "부서(팀)코드", nullable = false)
         private String deptCode;
-        
+
         @Schema(description = "부서(팀)명", example = "")
         private String deptNm;
-        
+
         @Schema(description = "상위 부서코드", example = "")
         private String upDeptCode;
-        
+
+        @Schema(description = "상위 부서명", example = "")
+        private String upDeptNm;
+
         @Schema(description = "순서", example = "")
         private int ordSeq;
-        
+
         @Schema(description = "회사코드", example = "")
         private String companyCode;
-        
+
         @Schema(description = "사용여부", example = "Y")
         private String useYn;
-        
+
         @Schema(description = "사용자권한 ID", example = "")
         private String userAuthId;
-        
+
         @Schema(description = "사용자권한명", example = "")
         private String userAuthNm;
-        
+
         @Schema(description = "관리자권한 ID", example = "")
         private String mgrAuthId;
-        
+
         @Schema(description = "관리자권한명", example = "")
         private String mgrAuthNm;
     }
-	
+
 	@Getter
     @ToString
     @Builder
@@ -63,10 +69,10 @@ public class DeptMgmtResDto {
     @AllArgsConstructor
     @Schema(description = "부서 목록 결과")
     public static class DeptsResult {
-        
+
         @Schema(description = "컨텐츠 정보", nullable = false)
         private List<Dept> contents;
-        
+
         @Schema(description = "검색 정보", nullable = false)
         private DeptMgmtReqDto.SearchDept search;
 
