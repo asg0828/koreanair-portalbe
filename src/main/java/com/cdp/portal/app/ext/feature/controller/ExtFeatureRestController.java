a128fa15801802cbaadaa0e0c8dc81c1171ae9a3
jmpark <pj.jmpark@kalmate.net>
2023-12-21 13:19:03 +0900

    Feature 전체 목록 조회(검색) API 추가 - 외부용

--- src/main/java/com/cdp/portal/app/ext/feature/controller/ExtFeatureRestController.java
+++ src/main/java/com/cdp/portal/app/ext/feature/controller/ExtFeatureRestController.java
@@ -9,12 +9,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cdp.portal.app.ext.feature.dto.request.ExtFeatureReqDto;
 import com.cdp.portal.app.ext.feature.dto.response.ExtFeatureSeparateResDto.ApiResExtFeatureSeparateResDtos;
 import com.cdp.portal.app.ext.feature.service.ExtFeatureSeparateService;
 import com.cdp.portal.app.ext.feature.service.ExtFeatureService;
+import com.cdp.portal.app.facade.feature.dto.request.FeatureReqDto;
+import com.cdp.portal.app.facade.feature.dto.response.FeatureResDto.ApiResFeatures;
 import com.cdp.portal.common.constants.CommonConstants;
 import com.cdp.portal.common.dto.ApiResDto;
 
@@ -68,6 +71,20 @@ public class ExtFeatureRestController {
         featureService.createFeature(dto);
         
         return ResponseEntity.ok(ApiResDto.success());
+    }
+    
+    @Operation(summary = "Feature 전체 목록 조회(검색)", description = "Feature 전체 목록을 검색한다.", tags = { "feature" })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = ApiResFeatures.class)))
+        }
+    )
+    @Parameter(name ="featureKoNm", required = false, description = "피쳐한글명", example = "")
+    @Parameter(name ="featureEnNm", required = false, description = "피쳐영문명", example = "")
+    @GetMapping(value = "/v1/features/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getFeaturesAll(
+            @RequestParam(value = "featureKoNm", required = false, defaultValue = "") String featureKoNm,
+            @RequestParam(value = "featureEnNm", required = false, defaultValue = "") String featureEnNm) {
+        return ResponseEntity.ok(ApiResDto.success(featureService.getFeatures(featureKoNm, featureEnNm)));
     }
 
 }
--- src/main/java/com/cdp/portal/app/ext/feature/service/ExtFeatureService.java
+++ src/main/java/com/cdp/portal/app/ext/feature/service/ExtFeatureService.java
@@ -1,9 +1,15 @@
 package com.cdp.portal.app.ext.feature.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.cdp.portal.app.ext.feature.dto.request.ExtFeatureReqDto;
+import com.cdp.portal.app.facade.feature.dto.request.FeatureReqDto;
+import com.cdp.portal.app.facade.feature.dto.response.FeatureResDto;
 import com.cdp.portal.app.facade.feature.mapper.FeatureMapper;
 import com.cdp.portal.app.facade.feature.model.FeatureModel;
 import com.cdp.portal.common.IdUtil;
@@ -48,6 +54,20 @@ public class ExtFeatureService {
                 .build();
         
         featureMapper.insert(featureModel);
+    }
+    
+    @Transactional(readOnly = true)
+    public List<FeatureResDto.Features> getFeatures(String featureKoNm, String featureEnNm) {
+        if (StringUtils.isEmpty(featureKoNm) && StringUtils.isEmpty(featureEnNm)) {
+            return new ArrayList<>();
+        }
+        
+        FeatureReqDto.SearchFeature searchDto = FeatureReqDto.SearchFeature.builder()
+                .featureKoNm(featureKoNm)
+                .featureEnNm(featureEnNm)
+                .build();
+        
+        return featureMapper.selectAll(searchDto);
     }
 
 }
